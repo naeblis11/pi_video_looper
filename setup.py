84e443f2da27cad27dad31da3f0285cac4fb6243
jason mcintyre <naeblis11@gmail.com>
2016-09-27 20:29:17 +0000

    updated author info

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 
 setup(name              = 'Adafruit_Video_Looper',
       version           = '1.0.0',
-      author            = 'Tony DiCola',
-      author_email      = 'tdicola@adafruit.com',
+      author            = 'Jason McIntyre',
+      author_email      = 'naeblis11@gmail.com',
       description       = 'Application to turn your Raspberry Pi into a dedicated looping video playback device, good for art installations, information displays, or just playing cat videos all day.',
       license           = 'GNU GPLv2',
-      url               = 'https://github.com/adafruit/pi_video_looper',
+      url               = 'https://github.com/naeblis11/pi_video_looper',
       install_requires  = ['pyudev'],
       packages          = find_packages())
